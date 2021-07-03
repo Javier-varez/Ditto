26a0ba334d6c562e1c6684dd90ee09a11c8e64d5
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-07-04 01:10:14 +0200

    Add Pair implementation

---
+++ inc/pair.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+template <class L, class R>
+class Pair {
+ public:
+  Pair(const L& l, const R& r) : m_left(l), m_right(r) {}
+
+  auto left() const -> const L& { return m_left; }
+
+  auto left() -> L& { return m_left; }
+
+  auto right() const -> const R& { return m_right; }
+
+  auto right() -> R& { return m_right; }
+
+ private:
+  L m_left;
+  R m_right;
+};
---
+++ test/pair.cpp
@@ -0,0 +1,24 @@
+
+#include "pair.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <string>
+
+TEST(PairTest, Constructor) {
+  Pair<int, std::string> pair{1, "cool!"};
+
+  EXPECT_EQ(pair.left(), 1);
+  EXPECT_STREQ(pair.right().c_str(), "cool!");
+
+  pair.left() = 10;
+  EXPECT_EQ(pair.left(), 10);
+}
+
+TEST(PairTest, ConstantPair) {
+  const Pair<int, std::string> pair{1, "cool!"};
+
+  EXPECT_EQ(pair.left(), 1);
+  EXPECT_STREQ(pair.right().c_str(), "cool!");
+}
