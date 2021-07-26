9f46783fe06936a14edaa5610e421066f8589aa6
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-07-26 14:41:17 +0200

    Implement circular buffer

---
+++ test/circular_buffer.cpp
@@ -0,0 +1,193 @@
+
+#include "ATE/circular_buffer.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using testing::Mock;
+using testing::StrictMock;
+
+class Whiny {
+ public:
+  MOCK_METHOD(void, constructor, (), ());
+  MOCK_METHOD(void, copy_constructor, (), ());
+  MOCK_METHOD(void, move_constructor, (), ());
+  MOCK_METHOD(void, copy_assignment_operator, (), ());
+  MOCK_METHOD(void, move_assignment_operator, (), ());
+  MOCK_METHOD(void, destructor, (), ());
+};
+
+class Element {
+ public:
+  Element(Whiny* whiny, int value) : m_whiny(whiny), m_value(value) {
+    m_whiny->constructor();
+  }
+
+  Element(Element&& other) : m_whiny(other.m_whiny), m_value(other.m_value) {
+    other.m_whiny = nullptr;
+    other.m_value = 0;
+    m_whiny->move_constructor();
+  }
+
+  Element(const Element& other)
+      : m_whiny(other.m_whiny), m_value(other.m_value) {
+    m_whiny->copy_constructor();
+  }
+
+  Element& operator=(Element&& other) {
+    if (this != &other) {
+      m_whiny = other.m_whiny;
+      m_value = other.m_value;
+      other.m_whiny = nullptr;
+      other.m_value = 0;
+      m_whiny->move_assignment_operator();
+    }
+    return *this;
+  }
+
+  Element& operator=(const Element& other) {
+    if (this != &other) {
+      m_whiny = other.m_whiny;
+      m_value = other.m_value;
+      m_whiny->copy_assignment_operator();
+    }
+    return *this;
+  }
+
+  ~Element() {
+    if (m_whiny) {
+      m_whiny->destructor();
+    }
+  }
+
+  operator int() const { return m_value; }
+
+ private:
+  Whiny* m_whiny{nullptr};
+  int m_value{0};
+};
+
+TEST(CircularBufferTest, EmplaceElement) {
+  StrictMock<Whiny> whiny;
+  CircularBuffer<Element, 10> buffer;
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 23));
+
+  ASSERT_NE(buffer.peek(), nullptr);
+  EXPECT_EQ(*buffer.peek(), 23);
+
+  EXPECT_CALL(whiny, destructor());
+}
+
+TEST(CircularBufferTest, PushElement) {
+  StrictMock<Whiny> whiny;
+  CircularBuffer<Element, 10> buffer;
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_TRUE(buffer.push({&whiny, 23}));
+
+  ASSERT_NE(buffer.peek(), nullptr);
+  EXPECT_EQ(*buffer.peek(), 23);
+
+  EXPECT_CALL(whiny, destructor());
+}
+
+TEST(CircularBufferTest, PopElement) {
+  StrictMock<Whiny> whiny;
+  CircularBuffer<Element, 10> buffer;
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 23));
+
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_CALL(whiny, destructor());
+  {
+    auto opt = buffer.pop();
+    EXPECT_TRUE(opt.has_value());
+    EXPECT_EQ(opt.value(), 23);
+  }
+
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_FALSE(buffer.pop().has_value());
+}
+
+TEST(CircularBufferTest, PopMultipleElements) {
+  StrictMock<Whiny> whiny;
+  CircularBuffer<Element, 10> buffer;
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 23));
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 32));
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_CALL(whiny, destructor());
+  EXPECT_EQ(buffer.pop().value(), 23);
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_CALL(whiny, destructor());
+  EXPECT_EQ(buffer.pop().value(), 32);
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_FALSE(buffer.pop().has_value());
+}
+
+TEST(CircularBufferTest, DoesntPushElementsWhenFull) {
+  StrictMock<Whiny> whiny;
+  CircularBuffer<Element, 2> buffer;
+
+  EXPECT_TRUE(buffer.empty());
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 23));
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 32));
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_TRUE(buffer.full());
+
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_CALL(whiny, destructor());
+  EXPECT_EQ(buffer.pop().value(), 23);
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, constructor());
+  EXPECT_TRUE(buffer.emplace(&whiny, 45));
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_CALL(whiny, destructor());
+  EXPECT_EQ(buffer.pop().value(), 32);
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_CALL(whiny, move_constructor());
+  EXPECT_CALL(whiny, destructor());
+  EXPECT_EQ(buffer.pop().value(), 45);
+  Mock::VerifyAndClearExpectations(&whiny);
+
+  EXPECT_TRUE(buffer.empty());
+  EXPECT_FALSE(buffer.pop().has_value());
+}
+
+TEST(CircularBufferTest, DestructorDestroysElementsInTheBuffer) {
+  constexpr std::size_t SIZE = 20;
+  StrictMock<Whiny> whiny;
+  CircularBuffer<Element, SIZE> buffer;
+
+  for (std::size_t i = 0; i < SIZE - 2; i++) {
+    EXPECT_CALL(whiny, constructor());
+    EXPECT_TRUE(buffer.emplace(&whiny, 23));
+    Mock::VerifyAndClearExpectations(&whiny);
+  }
+
+  EXPECT_CALL(whiny, destructor()).Times(SIZE - 2);
+}
