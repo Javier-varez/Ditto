5d55454f5f6da1b4f545d080d2efac64e8867310
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-07-29 02:16:08 +0200

    Add _Nonnull to NonNullPtr

--- inc/ATE/non_null_ptr.h
+++ inc/ATE/non_null_ptr.h
@@ -11,21 +11,21 @@ namespace ATE {
 template <class T>
 class NonNullPtr final {
  public:
-  NonNullPtr(T* ptr) : m_ptr(ptr) { ATE_VERIFY(m_ptr != nullptr); }
+  NonNullPtr(T* _Nonnull ptr) : m_ptr(ptr) { ATE_VERIFY(m_ptr != nullptr); }
 
   NonNullPtr(const NonNullPtr&) = default;
   NonNullPtr(NonNullPtr&&) = default;
   NonNullPtr& operator=(const NonNullPtr&) = default;
   NonNullPtr& operator=(NonNullPtr&&) = default;
 
-  T* get() const { return m_ptr; }
+  T* _Nonnull get() const { return m_ptr; }
 
-  T* operator->() const { return m_ptr; }
+  T* _Nonnull operator->() const { return m_ptr; }
 
   T& operator*() const { return *m_ptr; }
 
  private:
-  T* m_ptr;
+  T* _Nonnull m_ptr;
 };
 
 }  // namespace ATE
