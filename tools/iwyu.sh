fb9fb29bee0e28eaff5053e8091ace3f422d8298
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-08-01 10:27:53 +0200

    Fix tools

--- tools/iwyu.sh
+++ tools/iwyu.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -xe
 
-iwyu_tool.py -p . -- -Xiwyu --mapping_file=$PWD/tools/iwyu.imp | tee iwyu_report.txt
+SCRIPT_DIR=`dirname "$0"`
+
+pushd ${SCRIPT_DIR}/../build
+
+iwyu_tool.py -p . -- -Xiwyu --mapping_file=../tools/iwyu.imp | tee iwyu_report.txt
+
+popd
--- tools/iwyu_fix.sh
+++ tools/iwyu_fix.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -xe
 
-iwyu_tool.py -p . -- -Xiwyu --mapping_file=$PWD/tools/iwyu.imp | fix_includes.py
+SCRIPT_DIR=`dirname "$0"`
+
+pushd ${SCRIPT_DIR}/../build
+
+iwyu_tool.py -p . -- -Xiwyu --mapping_file=../tools/iwyu.imp | fix_includes.py
+
+popd
