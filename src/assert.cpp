ad63b63eae2e3de3b1173508b030e489634ece9f
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-08-03 20:07:39 +0200

    Exit on assert fail by default

--- src/assert.cpp
+++ src/assert.cpp
@@ -2,12 +2,14 @@
 #include "ditto/assert.h"
 
 #include <cstdio>
+#include <cstdlib>
 
 namespace Ditto {
 
 __attribute__((weak)) void assert_failed(const char* condition, int line,
                                          const char* file) {
   printf("Condition failed: %s at line: %d, file: %s\n", condition, line, file);
+  exit(EXIT_FAILURE);
 }
 
 }  // namespace Ditto
