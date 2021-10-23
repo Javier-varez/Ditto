e5848aedfdca18b2909b8cf7a57f878e50f9ac3f
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-10-23 17:36:26 +0200

    Improve fixed point tests
    FP64 construction was potentially flawed since the shift could trim
    the upper bits of the number to 32 bit or smaller.
    
    Also add round method

--- test/fixed.cpp
+++ test/fixed.cpp
@@ -79,3 +79,67 @@ TEST(FixedPointTest, ComparisonOperators) {
   EXPECT_FALSE(val >= 38);
   EXPECT_FALSE(val <= 37);
 }
+
+TEST(FixedPointTest, FP64_construct_from_integer) {
+  constexpr uint32_t value = 0xFFFFFFFF;
+  Ditto::FP64<16> fp64 = Ditto::FP64<16>::from_integer(value);
+  EXPECT_EQ(fp64.as_integer(), 0xFFFFFFFF);
+}
+
+TEST(FixedPointTest, FP64_addition_with_integer) {
+  constexpr uint32_t value = 0xFFFFFFFF;
+  Ditto::FP64<16> fp64 = Ditto::FP64<16>::from_integer(value);
+
+  auto result = fp64 + value;
+  EXPECT_EQ(result.as_integer(), 0x1FFFFFFFEULL);
+
+  result += value;
+  EXPECT_EQ(result.as_integer(), 0x2FFFFFFFDULL);
+}
+
+TEST(FixedPointTest, FP64_subtraction_with_integer) {
+  constexpr uint32_t value = 0xFFFFFFFF;
+  Ditto::FP64<16> fp64 = Ditto::FP64<16>::from_integer(0xFFFFFFFFFULL);
+
+  auto result = fp64 - value;
+  EXPECT_EQ(result.as_integer(), 0xF00000000ULL);
+
+  result -= value;
+  EXPECT_EQ(result.as_integer(), 0xE00000001ULL);
+}
+
+TEST(FixedPointTest, FP64_multiplication_with_integer) {
+  constexpr uint32_t value = 0xFFFFFFFFULL;
+  Ditto::FP64<16> fp64 = Ditto::FP64<16>::from_integer(1234);
+
+  auto result = fp64 * value;
+  EXPECT_EQ(result.as_integer(), 0x4D1FFFFFB2EULL);
+
+  result *= 2;
+  EXPECT_EQ(result.as_integer(), 0x4D1FFFFFB2EULL * 2);
+}
+
+TEST(FixedPointTest, FP64_division_with_integer) {
+  constexpr uint32_t value = 1234;
+  Ditto::FP64<16> fp64 = Ditto::FP64<16>::from_integer(0xFFFFFFFFFFULL);
+
+  auto result = fp64 / value;
+  EXPECT_EQ(result.as_integer(), 0x351BCC8DULL);
+
+  result /= 2;
+  EXPECT_EQ(result.as_integer(), 0x351BCC8DULL / 2);
+}
+
+TEST(FixedPointTest, round) {
+  FP32<8> val = FP32<8>::from_floating_point(10.5);
+  EXPECT_EQ(val.round().as_integer(), 11);
+  EXPECT_EQ(val.as_integer(), 10);
+
+  val = FP32<8>::from_floating_point(10.49);
+  EXPECT_EQ(val.round().as_integer(), 10);
+  EXPECT_EQ(val.as_integer(), 10);
+
+  val = FP32<8>::from_floating_point(10.51);
+  EXPECT_EQ(val.round().as_integer(), 11);
+  EXPECT_EQ(val.as_integer(), 10);
+}
