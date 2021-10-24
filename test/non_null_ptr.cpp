8f00d5b5572fae1440e938e17a01680bd0d8155a
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-10-24 15:24:24 +0200

    Add constructors from smart ptrs to NonNullPtr
    For convenience and type-erasure when passing pointers to a function.
    NonNullPtr does not own the memory it references.

--- test/non_null_ptr.cpp
+++ test/non_null_ptr.cpp
@@ -15,6 +15,34 @@ TEST(NonNullPtrTest, Nullptr) {
   StrictMock<Ditto::MockAssert> assert;
   Ditto::g_assert = &assert;
   EXPECT_CALL(assert, assert_failed(_, _, _));
-  Ditto::NonNullPtr<int> ptr{nullptr};
+  int* null_int = nullptr;
+  Ditto::NonNullPtr<int> ptr{null_int};
   Ditto::g_assert = nullptr;
+}
+
+TEST(NonNullPtrTest, FromRawPointer) {
+  int val = 0;
+  auto mutate = [](Ditto::NonNullPtr<int> int_ptr) { *int_ptr = 7; };
+
+  mutate(&val);
+
+  EXPECT_EQ(val, 7);
+}
+
+TEST(NonNullPtrTest, FromUniquePtr) {
+  std::unique_ptr<int> val_ptr = std::make_unique<int>(0);
+  auto mutate = [](Ditto::NonNullPtr<int> int_ptr) { *int_ptr = 7; };
+
+  mutate(val_ptr);
+
+  EXPECT_EQ(*val_ptr, 7);
+}
+
+TEST(NonNullPtrTest, FromSharedPointer) {
+  std::shared_ptr<int> val_ptr = std::make_shared<int>(0);
+  auto mutate = [](Ditto::NonNullPtr<int> int_ptr) { *int_ptr = 7; };
+
+  mutate(val_ptr);
+
+  EXPECT_EQ(*val_ptr, 7);
 }
