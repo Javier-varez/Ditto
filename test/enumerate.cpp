69fd5af5fb3fdd152b644f4846930c137d59a9d0
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-12-22 20:04:37 +0100

    Add test for map enumeration

--- test/enumerate.cpp
+++ test/enumerate.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <map>
+
 TEST(EnumerateTest, ItWorks) {
   std::vector<int> data{1, 2, 3, 4, 5};
 
@@ -16,6 +18,40 @@ TEST(EnumerateTest, ItWorks) {
   num_elements = 0;
   for (const auto& [index, val_ptr] : make_enumerated_iter(data)) {
     EXPECT_EQ(*val_ptr, 0);
+    num_elements++;
+  }
+  EXPECT_EQ(num_elements, data.size());
+}
+
+TEST(EnumerateTest, EnumeratedMap) {
+  std::map<std::string, int> data;
+  data.insert({"string1", 1});
+  data.insert({"string2", 2});
+  data.insert({"string3", 3});
+
+  size_t num_elements = 0;
+  for (auto& [index, val_ptr] : make_enumerated_iter(data)) {
+    const auto& str = val_ptr->first;
+    if (str == "string1") {
+      EXPECT_EQ(1, val_ptr->second);
+      EXPECT_EQ(0, index);
+    } else if (str == "string2") {
+      EXPECT_EQ(2, val_ptr->second);
+      EXPECT_EQ(1, index);
+    } else if (str == "string3") {
+      EXPECT_EQ(3, val_ptr->second);
+      EXPECT_EQ(2, index);
+    } else {
+      FAIL();
+    }
+    val_ptr->second = 0;
+    num_elements++;
+  }
+  EXPECT_EQ(num_elements, data.size());
+
+  num_elements = 0;
+  for (const auto& [index, val_ptr] : make_enumerated_iter(data)) {
+    EXPECT_EQ(val_ptr->second, 0);
     num_elements++;
   }
   EXPECT_EQ(num_elements, data.size());
