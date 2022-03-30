930c8dd93f64f815106af7b0fed232b95c14ef78
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-30 11:09:45 +0200

    Ditto::Pair forwarding constructor
    Allows the right element to be constructed by forwarding the
    arguments to the inner R constructor

--- include/ditto/pair.h
+++ include/ditto/pair.h
@@ -1,11 +1,19 @@
 #ifndef DITTO_PAIR_H_
 #define DITTO_PAIR_H_
 
+#include <utility>
+
 namespace Ditto {
 template <class L, class R>
 class Pair {
  public:
   Pair(const L& l, const R& r) : m_left(l), m_right(r) {}
+
+  // forwarding constructor for the right element
+  // Left element is still copied
+  template <class... T>
+  explicit Pair(const L& l, T... t)
+      : m_left(l), m_right(std::forward<T>(t)...) {}
 
   [[nodiscard]] auto left() const -> const L& { return m_left; }
 
